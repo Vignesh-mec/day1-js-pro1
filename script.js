839576f0b7913999afcc747ff2e267b53a576a2f
Vignesh-mec <v8481263@gmail.com>
2024-07-10 09:08:07 -0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('RCB').addEventListener('click', function() {
+    document.getElementById("image").src = "https://c.ndtvimg.com/2023-05/rc2q14f_virat-kohli_625x300_22_May_23.jpg"
+});
+document.getElementById('INDIA').addEventListener('click', function() {
+    document.getElementById("image").src = "https://th-i.thgim.com/public/incoming/ph9ch1/article67505974.ece/alternates/FREE_1200/GettyImages-1435855478.jpg"
+});
